84c43a2387150000a6eee97bdbca711ea97e43c0
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-11-23 20:57:29 -0600

    changes comment the compennts

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <TodoCounter />
-    <TodoSearch />
-    <TodoList>
-      <TodoItem />
-    </TodoList>
-    <CreateTodoButton />
+    // <TodoCounter />
+    // <TodoSearch />
+    // <TodoList>
+    //   <TodoItem />
+    // </TodoList>
+    // <CreateTodoButton />
   );
   }
 export default App
