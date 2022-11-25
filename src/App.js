e4645f27415e04365fb59b7c62298b4bd30016ec
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2022-11-24 20:45:04 -0600

    structuring project 24/11/22

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
-
+const todos = [
+  {text:'cut onion', completed:false},
+  {text:'take course intro react', completed:false},
+  {text:'cry', completed:false},
+]
 const App = () => {
   return (
-    // <TodoCounter />
-    // <TodoSearch />
-    // <TodoList>
-    //   <TodoItem />
-    // </TodoList>
-    // <CreateTodoButton />
+    <>
+    <TodoCounter />
+    <h2>Has completado 2 de 3 TODOs</h2>
+    <TodoSearch />
+    <input placeholder='Cebolla'/>
+    <TodoList>
+    {todos.map(todo => (
+    <TodoItem />
+    ))}
+    </TodoList>
+    <CreateTodoButton />
+    <button>+</button>
+    </>
   );
   }
 export default App
