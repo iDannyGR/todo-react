a871c004225c265282bc53d8ed65e218f2ea0452
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-01-09 20:36:04 -0600

    home 9/1/23

--- src/App/index.js
+++ src/App/index.js
@@ -1,54 +1,15 @@
-
 import './index.css';
 import { AppUI } from './AppUI';
 import { TodoProvider } from '../Context/TodoContext';
-import React, {useState} from 'react';
-
+import React from 'react';
 
 const App = () => {
-   const [first, setfirst] = useState('estado compartido');
   return (
     <>
-      <TodoHeader>
-      <TodoCounter />
-      <TodoSearch />
-      </TodoHeader>
-      <TodoList>
-          <TodoItem state={first} />
-      </TodoList>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
     </>
-  )
-}
-const TodoHeader = ({children}) => {
-  return (
-    <header>
-      {children}
-    </header>
-  )
-}
-const TodoList = ({children}) => {
-  return (
-      <section className='TodoList-container'>
-            {children}
-      </section>
-  )
-}
-const TodoCounter = () => {
-    return <p>TodoCounter</p>
-}
-const TodoSearch = () => {
-  return  <p>TodoSearch</p>
-}
-const TodoItem = ({state}) => {
-  return  <p>{state}</p>
-}
-// const App = () => {
-//   return (
-//     <>
-//     <TodoProvider>
-//       <AppUI />
-//     </TodoProvider>
-//     </>
-//   );
-//   }
-export default App
+  );
+  }
+export  { App }
